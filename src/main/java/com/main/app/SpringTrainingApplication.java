c1019234ca7e445b10a382ee91f128d77c330518
Misro Studio <misrostudio@gmail.com>
2023-02-26 19:33:11 +0700

    up

---
+++ src/main/java/com/main/app/SpringTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.main.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringTrainingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainingApplication.class, args);
+	}
+}
---
+++ src/main/java/com/main/app/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.main.app.repository;
+
+import com.main.app.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
